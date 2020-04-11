259bed64469c463776b825a521db18a667efab18
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-04-11 13:42:46 -0700

    removing unneeded codes on js file

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
 
     var ticketMasterAPI = "GzXQkPNDt7ZVTo3fbAmXPspPozArApCc";
     var keyword = ""; //search input
-    var city = "";
-    var genre = "Electronic"
+    var city = ""; //search city
+    var genre = "R&B" //search basen on genre
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city="+city+"&classificationName="+genre+"&keyword="+keyword+"&apikey="+ ticketMasterAPI;
-    // var queryURL = "https://app.ticketmaster.com/discovery/v2/classifications/genres/KnvZfZ7vAev?apikey="+ ticketMasterAPI;
 
     $.ajax({
         url: queryURL,
