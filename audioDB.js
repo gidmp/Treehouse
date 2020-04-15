37b747c9d05aecc2de940d1b9b7b9f0e73deebef
Vinh Mao <vinhxmao87@gmail.com>
2020-04-14 22:01:44 -0700

    more work on AudioDB API

--- audioDB.js
+++ audioDB.js
@@ -9,6 +9,22 @@ $.ajax({
     console.log(response);
     var artist = response.artists[0].strArtist;
     var bio = response.artists[0].strBiographyEN;
+    var artistID = response.artists[0].idArtist;
+    var musicBrainzArtist = response.artists[0].strMusicBrainzID;
+    var queryURL3 = "http://theaudiodb.com/api/v1/json/1/mvid.php?i=" + artistID;
+    $.ajax({
+        url: queryURL3,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+    var queryURL4 = "http://theaudiodb.com/api/v1/json/1/mvid-mb.php?i=" + musicBrainzArtist;
+    $.ajax({
+        url: queryURL4,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
 });
 
 $.ajax({
@@ -16,14 +32,16 @@ $.ajax({
     method: "GET"
 }).then(function(response) {
     console.log(response);
+    for(var i = 0; i < response.album.length; i++) {
+        console.log(response.album[i].strAlbum);
+    }
     var albumID = response.album[0].idAlbum;
-    var queryURL3 = "http://theaudiodb.com/api/v1/json/1/track.php?m=" + albumID;
+    var queryURL5 = "http://theaudiodb.com/api/v1/json/1/track.php?m=" + albumID;
     $.ajax({
-        url: queryURL3,
+        url: queryURL5,
         method: "GET"
     }).then(function(response) {
         console.log(response);
     });
-    
 });
 
