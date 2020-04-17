18b531ae903ebcaf642bfc4da268b5642265f726
Vinh Mao <vinhxmao87@gmail.com>
2020-04-16 20:52:49 -0700

    added script2.js file for code functionality

--- script2.js
+++ script2.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-
-
     $("#searchBtn").on("click", function(){
         event.preventDefault();
         var city = $("#location").val().trim();   
@@ -11,6 +9,53 @@ $(document).ready(function() {
 
 
     })
+
+    function getArtistInfo(artistName) {
+        console.log("Vinh's input: " + artistName);
+        var queryURL1 = "http://theaudiodb.com/api/v1/json/1/search.php?s=" + artistName;
+        var queryURL2 = "http://theaudiodb.com/api/v1/json/1/searchalbum.php?s=" + artistName;
+
+        $.ajax({
+            url: queryURL1,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var artist = response.artists[0].strArtist;
+            // var bio = response.artists[0].strBiographyEN;
+            var artistID = response.artists[0].idArtist;
+            var content = $("<p>").text(artist);
+            $("#placeHolderArt").append(content);
+            var queryURL3 = "http://theaudiodb.com/api/v1/json/1/mvid.php?i=" + artistID;
+            $.ajax({
+                url: queryURL3,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            });
+        });
+
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            for(var i = 0; i < response.album.length; i++) {
+                console.log(response.album[i].strAlbum);
+            }
+            var albumThumb = response.album[0].strAlbumThumb + "/preview";
+            var thumbContent = $("<img>").attr("src", albumThumb);
+            $("#placeHolderArt").append(thumbContent);
+            var albumID = response.album[0].idAlbum;
+            var queryURL5 = "http://theaudiodb.com/api/v1/json/1/track.php?m=" + albumID;
+            $.ajax({
+                url: queryURL5,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            });
+        });
+    }
+
 
     function inputQuery (city, genre) {
         var ticketMasterAPI = "GzXQkPNDt7ZVTo3fbAmXPspPozArApCc";
@@ -53,7 +98,7 @@ $(document).ready(function() {
                             $("#eventName").text(getTitle);
 
                             var artist = response._embedded.events[0]._embedded.attractions[0].name;
-                            $("#artistName").text("Artist: " + artist).attr("artist-name", artist);
+                            $("#artistName").text("Artist: " + artist);
 
                             var venueName = response._embedded.events[0]._embedded.venues[0].name;
                             var venueAddress = response._embedded.events[0]._embedded.venues[0].address.line1;
@@ -68,6 +113,7 @@ $(document).ready(function() {
                             var image = response._embedded.events[0]._embedded.attractions[0].images[4].url;
                             $("#eventImage").attr("src", image);
                             
+                            getArtistInfo(artist);
                     
             
                         })
